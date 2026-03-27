b06d2a8ccc997820ec98a74c3912505620b02e99
StevenCoverdale <S.coverdale@hotmail.com>
2026-03-26 23:23:21 -0400

    HomeVIew as a placeholdeer for now

---
+++ Taskly/Views/Main/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Taskly
+//
+//  Created by steven coverdale on 2026-03-26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+            
+            }
+            .navigationTitle("Taskly")
+        }
+    }
+}
+
